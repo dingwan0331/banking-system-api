dd39c6b071ccafb5c39a78f8b2efc87eb7820a0a
dingwan <dingwan0331@gmail.com>
2022-11-28 17:30:44 +0900

    feat: Django 프로젝트 생성
    - settings.py를 실행환경에 맞게 분리하기위해 settings폴더로 만들어 분리
    - App시간대 'Asia/Seoul'로 변경 USE_TZ False 설정
    - Django.admin & auth 와 불필요한 미들웨어 주석처리
    - 불필요한 TAMPLATE, AUTH_PASSWORD_VALIDATORS 및 기본주석 제거
    - urls.py admin경로 제거
    - 개발용 SECRET_KEY는 상수값으로 임시 설정
    - requirments.txt 에 django 버전 기재

---
+++ config/settings/development.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = 'a'
