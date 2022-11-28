e8d1f07a6be88c87f3ee0897b5881d0bf9ac5f58
dingwan <dingwan0331@gmail.com>
2022-11-28 20:23:30 +0900

    feat: Account Model 작성
    - TimeStampModel 상속받아서 작성

--- apps/bangking/models.py
+++ apps/bangking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from apps.util.models import TimeStampModel
+
 # Create your models here.
+
+class Account(TimeStampModel):
+    account_number = models.CharField(max_length=200)
+    balance        = models.DecimalField(max_digits=19, decimal_places=4)
+
+    class Meta():
+        db_table = 'accounts'
