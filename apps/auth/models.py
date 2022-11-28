657e13230ba32b9e170a802fd8f8a1e4e2f957b9
dingwan <dingwan0331@gmail.com>
2022-11-28 20:00:34 +0900

    feat: User Model 작성
    - TimeStampModel을 상속받아서 작성
    - __str__ 메서드 이름(pk)로 나오도록 오버라이드

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from apps.util.models import TimeStampModel
+
+class User(TimeStampModel):
+    name     = models.CharField(max_length=10)
+    password = models.BinaryField(max_length=60)
+    ssl      = models.CharField(max_length=60)
+
+    class Meta():
+        db_table = 'users'
+    
+    def __str__(self):
+        return f'{self.name} ({self.pk})'
