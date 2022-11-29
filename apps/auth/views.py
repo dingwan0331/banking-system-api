5e1cd7910e96d1d034567f3127e6f2bf0458c7c1
dingwan <dingwan0331@gmail.com>
2022-11-29 15:00:03 +0900

    feat: 로그인 기능 구현
    - urls.py설정 end-point = POST /auth/users/signin
    - ssn으로 회원조회
    - bcrypt로 암호화된 비밀번호 검증
    - 성공시 jwt 토큰 반납 SECRET_KEY는 임의로 상수로 설정, 알고리즘은 default알고리즘

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import bcrypt
+import jwt
+
+from django.views import View
+from django.http  import JsonResponse
+
+from .models         import User
+
+class SigninView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            ssn, password = data.get('ssn'), data.get('password')
+
+            user = User.objects.get(ssn = ssn)
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password):
+                return JsonResponse({"message" : "Password Invalid"}, status=401)
+
+            access_token = jwt.encode({'id':user.id}, 'SECRET_KEY')
+
+            return JsonResponse({'access_token' : access_token}, status=200)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'User does not exist'}, status=400)
+
+        except Exception:
+            return JsonResponse({'message' : 'Server Error'}, status=500)
