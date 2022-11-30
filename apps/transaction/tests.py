b133cb7390d8bdd9c9d8688839b157f9965245ff
dingwan <dingwan0331@gmail.com>
2022-12-01 01:12:15 +0900

    test: 입금api e2e suucess_case 테스트 추가(summary body에 없는경우)
    - client 인스턴스, jwt 토큰 전역변수로 선언
    - body값과 status_code 확인

--- apps/transaction/tests.py
+++ apps/transaction/tests.py
@@ -9,7 +9,9 @@ from .models              import Transaction, Account, AccountType
 from ..auth.models        import User
 from config.settings.base import SECRET_KEY
 
-balance = Decimal('100000.0000')
+client       = Client()
+balance      = Decimal('100000.0000')
+access_token = jwt.encode({'id':1}, SECRET_KEY)
 
 class DepositViewTest(TestCase):
     def setUp(self):
@@ -39,16 +41,35 @@ class DepositViewTest(TestCase):
         AccountType.objects.all().delete()
         User.objects.all().delete()
 
-    def test_deposit_success_case(self):
-        client       = Client()
-        access_token = jwt.encode({'id':1}, SECRET_KEY)
-        
+    def test_deposit_success_case(self):        
         request_body = { 
             'account_id' : 1,
             'password'   : '1234',
             'summary'    : '예금하기',
             'amount'     : 10000,
             }
+        headers = {
+            'HTTP_Authorization' : access_token,
+            'content_type'       :'application/json'
+        }
+
+        response = client.post('/transactions/deposit', request_body, **headers)
+
+        expected_response = {
+            'Balance after transaction' : str(balance + request_body['amount']), 
+            'Transaction amount'        : request_body['amount']
+            }
+
+        self.assertEqual(response.json(), expected_response)
+        self.assertEqual(response.status_code, 201)
+
+    def test_deposit_success_case_without_summary(self):
+        request_body = { 
+            'account_id' : 1,
+            'password'   : '1234',
+            'amount'     : 10000,
+        }
+
         headers = {
             'HTTP_Authorization' : access_token,
             'content_type'       :'application/json'
