b49027813962e2b52a929574a3788eea140f57b3
dingwan <dingwan0331@gmail.com>
2022-11-30 16:07:00 +0900

    feat: 토큰 검증 데코레이터 구현

---
+++ apps/util/token.py
@@ -0,0 +1,23 @@
+import jwt
+
+from django.http import JsonResponse
+
+from ..auth.models import User
+
+def validate_token(func):
+    def wrapper(self,request,*args,**kwargs):
+        try:
+            access_token = request.headers.get("Authorization",None)
+            payload = jwt.decode(access_token, 'SECRET_KEY', algorithms='HS256')
+            user = User.objects.get(id=payload['id'])
+            request.user = user  
+            
+            return func(self, request, *args, **kwargs)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'Invalid token'}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'Invalid token'}, status=401)
+
+    return wrapper
