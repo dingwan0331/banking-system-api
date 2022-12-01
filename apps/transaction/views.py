7f5dc69855aa68dcfcc011e7d82aa204695e8c2f
dingwan <dingwan0331@gmail.com>
2022-12-01 15:07:07 +0900

    refactor: TransactionView 리팩토링
    - body 값에 is_withdrawal를 boolean타입으로 받는것으로 변경
    - 응답헤더에 location 추가
    - 그 외 모든 body값 str타입으로 받기
    - is_withdrawal & summmary 타입검사
    - password 숫자 4자리인지 검사
    - amount 1이상의 양수인지 검사
    - account_id 0인지 검사
    - 출금일 경우 amount를 음수로 변경하는 로직 추가
    - 결제후 잔액이 0이하 이면 잔고 부족 에러핸들링 추가
    - 생성된 Transaction 레코드 변수명 transaction_row로 변경
    - response amount = transaction_row.amount로 수정

--- apps/transaction/views.py
+++ apps/transaction/views.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 import bcrypt
 
@@ -15,19 +16,41 @@ class TransactionView(View):
     def post(self, request, account_id):
         '''
         request = {
-            account_id: int,
+            account_id: str,
             password: str,
             summary: str,
-            amount: int,
+            amount: str,
+            is_withdrawal: boolean
         }
         '''
         try:
             data = json.loads(request.body)
             user = request.user
+
+            password      = data['password']
+            is_withdrawal = data['is_withdrawal']
+            amount        = data['amount']
+            summary       = data.get('summary',user.name)
+
+            PASSWORD_REGEX   = '\d{4}'
+            AMOUNT_REGEX     = '^[1-9]+(\.?[0-9]+)?$'
+                
+            if not re.fullmatch(AMOUNT_REGEX,amount):
+                return JsonResponse({'message' : 'Invalid amount'}, status=400)
             
-            password   = data['password']
-            amount     = data['amount']
-            summary    = data.get('summary',user.name)
+            if account_id == 0:
+                return JsonResponse({'message' : 'Invalid account_id'})
+
+            if type(is_withdrawal) != bool:
+                return JsonResponse({'message' : 'Invalid is_withdrawal'})
+            
+            if type(summary) != str:
+                return JsonResponse({'message' : 'Invalid summary'})
+
+            if not re.fullmatch(PASSWORD_REGEX,password):
+                return JsonResponse({'message' : 'Invalid password'})
+
+            signed_amount = -int(amount) if is_withdrawal else int(amount)
             
             with transaction.atomic(using='default'):
                 account = Account.objects.get(id = account_id)
@@ -38,20 +61,26 @@ class TransactionView(View):
                 if not bcrypt.checkpw(password.encode('utf-8') , account.password):
                     return JsonResponse({'message' : 'Invalid password'}, status=401)
 
-                deposit = Transaction.objects.create(
+                balacne = account.balance + signed_amount
+
+                if balacne < 0:
+                    return JsonResponse({'message' : 'Insufficient balance'})
+
+                transaction_row = Transaction.objects.create(
                     amount        = amount,
-                    balance       = account.balance + amount,
+                    balance       = balacne,
                     timestamp     = timezone.now().timestamp(),
-                    is_withdrawal = False,
+                    is_withdrawal = is_withdrawal,
                     summary       = summary,
                     account_id    = account_id
                 )
-                account.balance = deposit.balance
+                account.balance = transaction_row.balance
                 account.save()
 
-            result = {'Balance after transaction': account.balance, 'Transaction amount': amount}
+            result  = {'Balance after transaction': account.balance, 'Transaction amount': transaction_row.amount}
+            headers = {'Location': f'/transactions/{transaction_row.id}'}
 
-            return JsonResponse(result, status=201)
+            return JsonResponse(result,headers = headers, status=201)
 
         except Account.DoesNotExist:
             return JsonResponse({'message' : 'Does not account'}, status=400)
