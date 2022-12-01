0359b3d84459ad15313f774c92d4602f84b5a087
dingwan <dingwan0331@gmail.com>
2022-12-01 14:08:53 +0900

    refactor: DepositView TransactionView로 변경
    - urls.py import문 변경

--- apps/transaction/urls.py
+++ apps/transaction/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from apps.transaction.views import DepositView
+from apps.transaction.views import TransactionView
 
 urlpatterns = [
-    path('/deposit', DepositView.as_view())
+    path('/deposit', TransactionView.as_view())
 ]
