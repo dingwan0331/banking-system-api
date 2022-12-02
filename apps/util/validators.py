89754ad47d19103f48f2d0bfc484af875482b2bb
dingwan <dingwan0331@gmail.com>
2022-12-02 19:56:13 +0900

    refactor: 입,출금 api PostTransactionsJsonTransform class로 validation
    - 해당 키가 없을시 Key Error가 아닌 validation으로 처리

---
+++ apps/util/validators.py
@@ -0,0 +1,43 @@
+import re
+import json
+
+from django.core.exceptions import ValidationError
+
+class PostTransactionsJsonValidator:
+    def __init__(self, body_json):
+        data = json.loads(body_json)
+
+        self.password      = data.get('password', '')
+        self.summary       = data.get('summary', '')
+        self.amount        = data.get('amount', '')
+        self.is_withdrawal = data.get('is_withdrawal', '')
+
+        self.__validate()
+
+    def __validate(self):
+        self.__validate_is_withdrawal()
+        self.__validate_password()
+        self.__validate_amount()
+        self.__validate_summary()
+
+    def __validate_password(self):
+        PASSWORD_REGEX = '\d{4}'
+        
+        if not re.fullmatch(PASSWORD_REGEX, self.password):
+            raise ValidationError('Invalid password')            
+
+    def __validate_is_withdrawal(self):
+        if type(self.is_withdrawal) != bool:
+            raise ValidationError('Invalid is_withdrawal')
+    
+    def __validate_summary(self):
+        if self.summary and type(self.summary) != str:
+            raise ValidationError('Invalid summary')
+
+    def __validate_amount(self):
+        AMOUNT_REGEX = '^[1-9]+(\.?[0-9]+)?$'
+            
+        if not re.fullmatch(AMOUNT_REGEX,self.amount):
+            raise ValidationError('Invalid amount')
+
+        self.amount = -int(self.amount) if self.is_withdrawal else int(self.amount)
