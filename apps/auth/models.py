47582b5e1b32ca3291b652fa86feb50c7035e60a
dingwan <dingwan0331@gmail.com>
2022-12-02 22:30:22 +0900

    refactor: User 모델에 credit 추가
    - 입출금된 돈은 User모델의 credit으로 흘러가게 추가
    - default=0

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -3,10 +3,11 @@ from django.db import models
 from apps.util.models import TimeStampModel
 
 class User(TimeStampModel):
-    first_name         = models.CharField(max_length=5)
-    last_name          = models.CharField(max_length=5)
-    username           = models.CharField(max_length=10, unique=True)
-    password           = models.BinaryField(max_length=60)
+    first_name = models.CharField(max_length=5)
+    last_name  = models.CharField(max_length=5)
+    username   = models.CharField(max_length=10, unique=True)
+    password   = models.BinaryField(max_length=60)
+    credit     = models.PositiveIntegerField(default=0)
 
     class Meta():
         db_table = 'users'
