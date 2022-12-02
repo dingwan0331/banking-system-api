020fd45ba2a76c5f5a8c355dd850e012c60deea9
dingwan <dingwan0331@gmail.com>
2022-12-02 18:22:32 +0900

    refactor: User 모델 리팩토링
    - last_name, first_name 추가
    - ssn대신 식별자로 username 설정(유니크속성)

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -3,9 +3,10 @@ from django.db import models
 from apps.util.models import TimeStampModel
 
 class User(TimeStampModel):
-    name     = models.CharField(max_length=10)
-    password = models.BinaryField(max_length=60)
-    ssn      = models.BinaryField(max_length=256)
+    first_name         = models.CharField(max_length=5)
+    last_name          = models.CharField(max_length=5)
+    username           = models.CharField(max_length=10, unique=True)
+    password           = models.BinaryField(max_length=60)
 
     class Meta():
         db_table = 'users'
