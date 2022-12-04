c0a74f03be08f8c381998d709103822f9f278f25
dingwan <dingwan0331@gmail.com>
2022-12-04 14:33:11 +0900

    chore: settings/production.py 추가
    - DEBUG=False설정
    - DB정보 환경변수로 관리
    - ALLOWED_HOSTS['*']

---
+++ config/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("PRODUCTION_DATABASE_ENGINE"),
+        'NAME': os.environ.get("PRODUCTION_DATABASE_NAME"),
+    }
+}
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
